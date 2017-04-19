8cd4986e68665e5a62747ca85fb15b74642d9f75
marco.rodrigues <mrodrigues@inegi.up.pt>
2017-04-19 14:24:15 +0100

    Mouse Up 1

--- main.js
+++ main.js
@@ -4,17 +4,46 @@ var canvas = document.getElementById('canvas'),
     vx = 1,
     vy = 1;
 
-canvas.style.background = '#ead5ac';
+canvas.style.background = '#66ff66';
 
-ball.x = canvas.width/4;
-ball.y = canvas.height*3/4;
-//ball.draw(context);
+ball.x = getRandomArbitrary(0,canvas.width);
+ball.y = getRandomArbitrary(0,canvas.height);
 
-(function drawFrame(){
+ball.draw(context);
+
+canvas.addEventListener('mouseup', drawFrame, false);
+
+function drawFrame(){
     window.requestAnimationFrame(drawFrame, canvas);
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    var left = 0,
+    right = canvas.width,
+    top = 0,
+    bottom = canvas.height;
+
     ball.x += vx;
     ball.y += vy;
-    ball.draw(context);
-})();
+
+	if (ball.x + ball.radius > right) {
+      ball.x = right - ball.radius;
+      vx *= -1;
+    } else if (ball.x - ball.radius < left) {
+      ball.x = left + ball.radius;
+      vx *= -1;
+    }
+    if (ball.y + ball.radius > bottom) {
+      ball.y = bottom - ball.radius;
+      vy *= -1;
+    } else if (ball.y - ball.radius < top) {
+      ball.y = top + ball.radius;
+      vy *= -1;
+    }
+
+    ball.draw(context);
+};
+
+
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
